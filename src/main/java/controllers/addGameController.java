f8cfa000ba6a67f087355392f877e4226f856d3d
SilasParker <silas.parker@gmail.com>
2021-02-11 18:05:03 +0000

    Worked on the Add Game Form
    addGameController.java: Initialised all the FXML properties and started working on the submit function
    addGame.fxml: Created the first draft of the layout for this screen

--- src/main/java/controllers/addGameController.java
+++ src/main/java/controllers/addGameController.java
@@ -3,14 +3,42 @@ package src.main.java.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class addGameController implements Initializable {
+
+    @FXML private TextField gameName, charNameEntry, mapNameEntry;
+    @FXML private Button gameImage, charNameAdd, mapNameAdd, submit, cancel;
+    @FXML private RadioButton charRadio1, charRadio2, charRadio3, mapRadio1, mapRadio2, teammateRadio1, teammateRadio2;
+    @FXML private ListView<HBox> charListView, mapListView;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         // TODO Auto-generated method stub
 
+    }
+
+    public void submit(ActionEvent event) {
+        String subGameName = gameName.getText();
+        //Path
+        int subCharNumPerSide = 1;
+        if(charRadio2.isSelected()) {
+            subCharNumPerSide = 2;
+        } else if(charRadio3.isSelected()) {
+            subCharNumPerSide = 3;
+        }
+        boolean subMap = true;
+        if(mapRadio2.isSelected()) {
+            subMap = false;
+        }
+        
     }
     
 }
