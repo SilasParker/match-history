fe66edafe3973b7bc552f40e4a9041027caab700
SilasParker <silas.parker@gmail.com>
2021-03-16 16:17:26 +0000

    Main Screen Functionalities
    mainController.java: Main screen now correctly sets game image&name as well as centering the image
    
    main.fxml: Added the backToGameSelect function to the Back button

--- src/main/java/controllers/mainController.java
+++ src/main/java/controllers/mainController.java
@@ -1,18 +1,27 @@
 package src.main.java.controllers;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import src.main.java.Game;
 
 public class mainController implements Initializable {
 
-    @FXML ImageView gameImageView;
+    @FXML private ImageView gameImageView;
+    @FXML private Label gameNameLabel;
 
     private Game game;
 
@@ -27,9 +36,43 @@ public class mainController implements Initializable {
 
         String imageDirectory = "/src/local/games/"+game.toDirectorySafeString(game.getName())+".png";
         System.out.println(imageDirectory);
-        File file = new File("/src/local/games/"+game.toDirectorySafeString(game.getName())+".png");
-        Image image = new Image(file.toURI().toString());
-        gameImageView.setImage(image);
+
+        gameImageView.setImage(new Image("src/local/games/"+game.toDirectorySafeString(game.getName())+"/"+game.toDirectorySafeString(game.getName())+".png"));
+        centerImageInImageView(gameImageView);
+        System.out.println(gameImageView.getX()+","+gameImageView.getY());
+        gameNameLabel.setText(game.getName());
+    }
+
+    private void centerImageInImageView(ImageView imgView) {
+        Image img = imgView.getImage();
+        double w = 0;
+        double h = 0;
+        double xRatio = imgView.getFitWidth()/img.getWidth();
+        double yRatio = imgView.getFitHeight()/img.getHeight();
+        double reductionCoefficient = 0;
+        if(xRatio >= yRatio) {
+            reductionCoefficient = yRatio;
+        } else {
+            reductionCoefficient = xRatio;
+        }
+        w = img.getWidth()*reductionCoefficient;
+        h = img.getHeight()*reductionCoefficient;
+        imgView.setX((imgView.getFitWidth()-w)/2);
+        imgView.setY((imgView.getFitHeight()-h)/2);
+        
+
+    }
+
+    public void backToGameSelect(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        URL fxmlURL = getClass().getResource("/src/resources/fxml/gameSelect.fxml");
+        loader.setLocation(fxmlURL);
+        Parent gameSelectViewParent = loader.load();
+        Scene gameSelectViewScene = new Scene(gameSelectViewParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(gameSelectViewScene);
+        window.show();
     }
     
 }
